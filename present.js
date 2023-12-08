1f830e1c0b9cc3226f2c0192e08abe5bde79366f
sfhfrry <153242833+sfhfrry@users.noreply.github.com>
2023-12-08 07:01:36 +0700

    Add files via upload
    create card

---
+++ present.js
@@ -0,0 +1,45 @@
+
+var to = 'Friendly!';
+var gift_url = 'https://www.facebook.com/profile.php?id=100027626430701&locale=vi_VN';
+var gift_image_url = '318128086_1213848089546061_800556986945856160_n.jpg';
+
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
